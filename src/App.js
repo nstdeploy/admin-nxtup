0aca623984992006d56007413b607de69f3de65e
Ved <ved@gmail.com>
2025-01-13 21:31:14 +0530

    seSEd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 import login from "./pages/login/login";
+import AddEvents from "./pages/add-events/events"
 
 function App() {
   return (
@@ -8,8 +9,8 @@ function App() {
           <BrowserRouter>
             <Routes>
               <Route path="login" Component={login}/>
-              <Route></Route>
-              <Route></Route>
+              <Route path="/addevents" element={<AddEvents/>}></Route>
+              <Route ></Route>
             </Routes>
           </BrowserRouter>
       </div>
