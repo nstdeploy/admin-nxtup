037e006058a2494304b0d4d7c1b80a1ef67abebb
Rohan <r0hansingh@Rohans-MacBook-Pro.local>
2025-01-14 03:05:47 +0530

    style: improve UI for admin and login page

--- src/pages/add-events/events.jsx
+++ src/pages/add-events/events.jsx
@@ -30,112 +30,119 @@ const AddEvents = () => {
   };
 
   return (
-    <div className="min-h-screen bg-black text-white flex items-center justify-center">
-      <div className="max-w-4xl w-full bg-gray-800 p-8 rounded-lg shadow-lg">
-        <h1 className="text-3xl font-bold mb-6 text-center">Add New Event</h1>
+    <div className="flex items-center justify-center min-h-screen text-white bg-black">
+      <div className="w-full max-w-4xl p-8 rounded-lg shadow-lg bg-zinc-900">
+        <h1 className="mb-6 text-3xl font-bold text-center">Add New Event</h1>
         <form onSubmit={handleSubmit} className="space-y-4">
           <div>
-            <label className="block text-sm font-medium">Title</label>
+            <label className="block mb-1 font-medium text-md">Title</label>
             <input 
               type="text" 
               name="title" 
+              placeholder="Enter event title"
               value={eventData.title} 
               onChange={handleChange} 
-              className="w-full mt-1 p-2 bg-gray-700 text-white rounded-md border border-gray-600" 
+              className="w-full p-2 pl-3 mt-1 text-black bg-white rounded-md" 
             />
           </div>
           <div>
-            <label className="block text-sm font-medium">Description</label>
+            <label className="block mt-6 mb-1 font-medium text-md">Description</label>
             <textarea 
               name="description" 
+              placeholder="Give your event a description"
               value={eventData.description} 
               onChange={handleChange} 
-              className="w-full mt-1 p-2 bg-gray-700 text-white rounded-md border border-gray-600 h-32"
+              className="w-full h-32 p-2 pl-3 mt-1 text-black bg-white rounded-md"
             ></textarea>
           </div>
           <div>
-            <label className="block text-sm font-medium">Banner Image</label>
+            <label className="block mt-6 mb-1 font-medium text-md">Banner Image</label>
             <input 
               type="file" 
               name="banner" 
               onChange={handleFileChange} 
-              className="mt-1 text-white" 
+              className="mt-1" 
             />
           </div>
           <div>
-            <label className="block text-sm font-medium">Date</label>
+            <label className="block mt-6 mb-1 font-medium text-md">Date</label>
             <input 
               type="date" 
               name="date" 
               value={eventData.date} 
               onChange={handleChange} 
-              className="w-full mt-1 p-2 bg-gray-700 text-white rounded-md border border-gray-600" 
+              className="w-full p-2 mt-1 text-black bg-white rounded-md" 
             />
           </div>
           <div>
-            <label className="block text-sm font-medium">Time</label>
+            <label className="block mt-6 mb-1 font-medium text-md">Time</label>
             <input 
               type="time" 
               name="time" 
               value={eventData.time} 
               onChange={handleChange} 
-              className="w-full mt-1 p-2 bg-gray-700 text-white rounded-md border border-gray-600" 
+              className="w-full p-2 mt-1 text-black bg-white rounded-md" 
             />
           </div>
           <div>
-            <label className="block text-sm font-medium">Location</label>
+            <label className="block mt-6 mb-1 font-medium text-md">Location</label>
             <input 
               type="text" 
               name="location" 
+              placeholder="Enter event location"
               value={eventData.location} 
               onChange={handleChange} 
-              className="w-full mt-1 p-2 bg-gray-700 text-white rounded-md border border-gray-600" 
+              className="w-full p-2 pl-3 mt-1 text-black bg-white rounded-md" 
             />
           </div>
           <div>
-            <label className="block text-sm font-medium">Organizer</label>
+            <label className="block mt-6 mb-1 font-medium text-md">Organizer</label>
             <input 
               type="text" 
-              name="organizer" 
+              name="organizer"
+              placeholder="Who is organizing this event?" 
               value={eventData.organizer} 
               onChange={handleChange} 
-              className="w-full mt-1 p-2 bg-gray-700 text-white rounded-md border border-gray-600" 
+              className="w-full p-2 pl-3 mt-1 text-black bg-white rounded-md" 
             />
           </div>
           <div>
-            <label className="block text-sm font-medium">Prize Worth</label>
+            <label className="block mt-6 mb-1 font-medium text-md">Prize Worth</label>
             <input 
               type="text" 
               name="prizeWorth" 
+              placeholder="Enter Prize"
               value={eventData.prizeWorth} 
               onChange={handleChange} 
-              className="w-full mt-1 p-2 bg-gray-700 text-white rounded-md border border-gray-600" 
+              className="w-full p-2 pl-3 mt-1 text-black bg-white rounded-md" 
             />
           </div>
           <div>
-            <label className="block text-sm font-medium">Cost</label>
+            <label className="block mt-6 mb-1 font-medium text-md">Cost</label>
             <input 
               type="text" 
               name="cost" 
+              placeholder="Enter registration fee"
               value={eventData.cost} 
               onChange={handleChange} 
-              className="w-full mt-1 p-2 bg-gray-700 text-white rounded-md border border-gray-600" 
+              className="w-full p-2 pl-3 mt-1 text-black bg-white rounded-md" 
             />
           </div>
           <div>
-            <label className="block text-sm font-medium">Team Size</label>
+            <label className="block mt-6 mb-1 font-medium text-md">Team Size</label>
             <input 
               type="text" 
               name="teamSize" 
+              placeholder="Enter team size"
               value={eventData.teamSize} 
               onChange={handleChange} 
-              className="w-full mt-1 p-2 bg-gray-700 text-white rounded-md border border-gray-600" 
+              className="w-full p-2 pl-3 mt-1 text-black bg-white rounded-md" 
             />
           </div>
           <div>
             <button 
               type="submit" 
-              className="w-full mt-4 p-2 bg-blue-600 hover:bg-blue-500 rounded-md text-white font-medium"
+              className="w-32 pt-3 pb-3 pl-6 pr-6 mt-6 font-medium text-white bg-zinc-600 rounded-lg ml-[84.5%] hover:bg-zinc-700 duration-300"
             >
               Add Event
             </button>
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -5,7 +5,7 @@ import Nav from "../../components/nav";
 
 function Login() {
 
-    const ForgotPass = () => toast.error("Kindly Contact Your Admin !")
+    const ForgotPass = () => toast.error("Kindly Contact Your Admin!")
     
     const navigate = useNavigate();
 
@@ -50,24 +50,24 @@ function Login() {
     }
 
     return(
-    <div className="bg-black text-white">
+    <div className="text-white bg-black">
         
         <Nav/>
-        <div className="flex items-center justify-center scale-80 pt-16 pb-12">
+        <div className="flex items-center justify-center pt-16 pb-12 scale-75">
             <img src="assets/nst.svg" alt="NST"/>
-            <h1 className="text-3xl font-extrabold ml-5 mr-5 rotate-45">+</h1>
+            <h1 className="ml-5 mr-5 text-3xl font-extrabold rotate-45">+</h1>
             <img src="assets/ru.svg" alt="RU"/>
         </div>
         <div className="flex items-center justify-center pb-20">
-            <form className="m-10 p-5 bg-zinc-900  w-[650px] flex flex-col items-center justify-center rounded-3xl " onSubmit={sendLoginData}>
-                <div className="flex flex-col mt-12 mb-8">
-                    <label id="email" className="pl-1">Email :</label>
+            <form className="m-10 p-5 bg-zinc-900  w-[575px] flex flex-col items-center justify-center rounded-3xl " onSubmit={sendLoginData}>
+                <div className="flex flex-col mt-10 mb-8">
+                    <label id="email">Email: </label>
                     <div className="flex">
                         <input 
                             placeholder="Enter your email" 
                             type="text"
                             htmlFor="email"
-                            className="h-10 w-[450px] rounded-l-md text-black p-3 bg-white"
+                            className="h-10 w-[28.125rem] rounded-md text-black p-3 bg-white mt-2"
                             onChange={handleChange}
                             required
                             value={formData.email}
@@ -77,25 +77,25 @@ function Login() {
                     </div>
                 </div>
                 <div className="flex flex-col mb-4">
-                    <label id="password" className="pl-1">Password: </label>
+                    <label id="password">Password: </label>
                     <input
                         placeholder="Enter your password"
                         type="password"
                         htmlFor="password"
-                        className="h-10 w-[450px] rounded-md text-black p-3"
+                        className="h-10 w-[28.125rem] rounded-md text-black p-3 bg-white mt-2" 
                         onChange={handleChange}
                         required
                         value={formData.pass}
                         name="Password"
                     />
                 </div>
-                <div className="mb-10 ml-[50%]">
-                    <span onClick={ForgotPass} className="cursor-pointer">Forgot Password?</span>
+                <div className="mb-10 ml-[60%]">
+                    <span onClick={ForgotPass} className="duration-300 cursor-pointer hover:underline hover:text-gray-400">Forgot Password?</span>
                 </div>
                 
                     <button
                         type="submit"
-                        className="mb-10 mt-5 bg-zinc-600 h-[40px] w-[400px] flex items-center justify-center rounded-xl cursor-pointer">Submit
+                        className="mb-10 bg-zinc-600 h-[40px] w-[28.125rem] flex items-center justify-center rounded-lg cursor-pointer hover:bg-zinc-700 duration-300 hover:text-gray-300">Submit
                     </button>
                 
             </form>
