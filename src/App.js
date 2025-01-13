c112ec0750fa9bc036001b642c88476d64a164fa
Shreyansh Agrawal <shreyanshagrawal@Shreyanshs-MacBook-Pro.local>
2025-01-13 20:55:21 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import login from "./pages/login/login";
 
 function App() {
-  return <div className="bg-black"></div>;
+  return (
+      <div className="bg-black">
+          <BrowserRouter>
+            <Routes>
+              <Route path="login" Component={login}/>
+              <Route></Route>
+              <Route></Route>
+            </Routes>
+          </BrowserRouter>
+      </div>
+  );
 }
 
 export default App;
---
+++ src/pages/login/login.jsx
@@ -0,0 +1,13 @@
+
+
+function login() {
+    return(
+    <div className="bg-black text-white">
+        <div>
+            <img src="../../../public/assets/nXTUP.svg" alt="Logo"/>
+        </div>
+    </div>
+    )
+}
+
+export default login
